a25fe155fd646d81bcf3578f3453dd3cf4e457be
wheezyg <83402672+wheezyg@users.noreply.github.com>
2022-04-10 11:20:02 +0800

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ password = input("Password:")
 
 #VARIABLES
 computer = random.randint(1,20)
-turns = 0
+turns = 6
+goes = 0
 
 print ("Welcome back", email)
 
@@ -17,20 +18,25 @@ user = int(input("Guess what number the computer is thinking of between 1 and 20
 while user:
     if user != computer and user > computer:
         print ("incorrect, number is lower")
-        turns += 1
+        turns -= 1
+        goes += 1
         print ("you have", turns, "goes left")
         user = int(input("guess again\n"))
 
     if user != computer and user < computer:
         print ("incorrect, number is higher")
-        turns += 1
+        turns -= 1
+        goes += 1
         print ("you have", turns, "goes left")
         user = int(input("guess again\n"))
     if user == computer:
+        goes += 1
         print ("CORRECT\nyou got it in", turns, "goes")
         break
-    if turns == 6:
+    if turns == 1:
+        print ("incorrect")
         print("out of goes")
+        print ("GAME OVER")
         break
 
 
